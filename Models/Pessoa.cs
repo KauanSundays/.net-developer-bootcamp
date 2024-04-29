c0d71584feff91d805aec4d57456f46c78f6d56d
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-04-29 09:02:18 -0300

    Passando parametros para o modelo Pessoa

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -10,7 +10,12 @@ namespace dotnet_bootcamp.Models
         public string Nome { get; set; }
         public int Idade { get; set; }
 
-        public void  Apresentar() 
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
         }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(10 + 2);
+﻿using System;
+using dotnet_bootcamp.Models;
+
+namespace dotnet_bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("MC Poze");
+            
+            pessoa1.Idade = 12;
+            pessoa1.Apresentar();
+        }
+    }
+}
