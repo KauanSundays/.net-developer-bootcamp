39999d495645f6877b585b77844252af1e98f3b2
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-05-03 07:35:31 -0300

    desafio-concluido

--- tipo-de-dados/obj/Debug/net8.0/tipo-de-dados.AssemblyInfo.cs
+++ tipo-de-dados/obj/Debug/net8.0/tipo-de-dados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tipo-de-dados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a94bdec583134e929a8af38f49ad6b87882ee1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e62bb1f8e5576ce7ddd65caff55172f22d79191")]
 [assembly: System.Reflection.AssemblyProductAttribute("tipo-de-dados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tipo-de-dados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
